d4a42ee996ffa93b0feb015afc255617409a2fcb
Nargiz Mamedova <nargizm7@yandex.ru>
2021-12-20 23:26:36 +0300

    HW6. passport.js added

--- src/app.js
+++ src/app.js
@@ -4,14 +4,59 @@ import expressWinston from 'express-winston';
 import { userRouter } from "./routers/user-router.js";
 import { groupRouter } from "./routers/group-router.js";
 import { userGroupRouter } from "./routers/usergroup-router.js";
-import { winstonInstance } from "./helpers/winston-logger.js"
+import { winstonInstance } from "./helpers/winston-logger.js";
+import passport from "passport";
+import { v4 as uuidv4 } from "uuid";
+import _ from "lodash";
+import { Strategy } from "passport-local";
+import BearerStrategy from "passport-http-bearer";
+import employees from './employees.json'
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+const tokens = [];
 
 app.use(expressWinston.logger({ winstonInstance }));
 app.use(express.json());
+
+passport.use(new Strategy(
+  {session: false},
+  (username, password, done) => {
+    const employee = _.find(employees, { firstName : username })
+    if(employee === undefined || employee.lastName !== password) {
+      return done(null, false, "Bad credentials");
+    }
+    return done(null, employee);
+  }
+));
+
+passport.use(new BearerStrategy((token, done) => {
+  const result = _.find(tokens, {token});
+
+  if(result === undefined) {
+    return done(null, false);
+  }
+  return done(null, result)
+}));
+
+app.use(passport.initialize());
+
+app.post('/authenticate', passport.authenticate('local', { session: false }), 
+  (req, res) => {
+    const user = req.user;
+    const token = { id: user.id, token: uuidv4() }
+    tokens.push(token);
+    res.json(token);
+  }
+);
+
+app.get('/employees', passport.authenticate('bearer', { session: false }),
+  (req, res) => {
+    res.json(employees)
+  }
+);
+
 app.use(userRouter);
 app.use(groupRouter);
 app.use(userGroupRouter);
@@ -21,6 +66,11 @@ process.on('unhandledRejection', error => {
 }).on('uncaughtException', error => {
   winstonInstance.error(`uncaughtException: ${error.message}`);
   process.exit(1);
+});
+
+app.use((err, req, res, next) => {
+  winstonInstance.error(`Unhandled error: ${err.message}`);
+  res.status(500).send("Internal Server Error")
 });
 
 app.listen(port, () => winstonInstance.info(`Server is listening on port ${port}`));
