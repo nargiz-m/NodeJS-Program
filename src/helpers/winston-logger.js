ffe1a0a0134a4af523e1ba7abc745e0d7a28b18b
Nargiz Mamedova <nargizm7@yandex.ru>
2021-12-21 02:42:40 +0300

    HW5. Logging into file added

--- src/helpers/winston-logger.js
+++ src/helpers/winston-logger.js
@@ -1,13 +1,13 @@
 import winston from 'winston';
 
 export const winstonInstance = winston.createLogger({
-  transports: new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.timestamp({format: 'DD.MM.YYYY HH:mm:ss'}),
-      winston.format.printf(
-       info => `${info.timestamp}: ${info.level} - ${info.message}`
-      )
-    )
-  })
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/programLogs.log' })
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({format: 'DD.MM.YYYY HH:mm:ss'}),
+    winston.format.printf(info => `${info.timestamp}: ${info.level} - ${info.message}`)
+  )
 });
